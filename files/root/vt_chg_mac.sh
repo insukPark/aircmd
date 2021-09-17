fb2aee570d7ed4d8919ec0cde0d1d7aa56eafe87
Ubuntu 64-bit 20.04.2.0 <ispark65@naver.com>
2021-09-17 11:40:01 +0900

    [upgrade] using ethaddr variable after while{..}

--- files/root/vt_chg_mac.sh
+++ files/root/vt_chg_mac.sh
@@ -1,45 +1,44 @@
 #!/bin/sh
-#/root/vt_chg_mac.sh
 FILE="/etc/fw_env.config"
+TMPFILE="/var/vttmp"
 
 if [ ! -e $FILE ];then
-        echo "#dev-name offset    env-size" > /etc/fw_env.config
-        echo "/dev/mtd1 0x30000 0x1000" >> /etc/fw_env.config
+	echo "#dev-name offset    env-size" > /etc/fw_env.config
+	echo "/dev/mtd1 0x30000 0x1000" >> /etc/fw_env.config
 fi
 
-fw_printenv | while IFS='=' read -r f1 eth
+fw_printenv > $TMPFILE
+while IFS='=' read -r f1 eth
 do
-        if [ "$f1" == ethaddr ]; then
-#               echo "PARKIS ethaddr=$eth" > /dev/console
-#               echo "PARKIS ethaddr="$eth"" > /dev/console
-#               echo "PARKIS ethaddr=${eth}" > /dev/console
+	if [ "$f1" == ethaddr ]; then
+		lanmac=$eth
+	elif [ "$f1" == wanaddr ]; then
+		wanmac=$eth
+	elif [ "$f1" == ra0addr ]; then
+		ra0mac=$eth
+	elif [ "$f1" == ra1addr ]; then
+		ra1mac=$eth
+	fi
+done < $TMPFILE
 
-                if [ ${#eth} != 17 ]; then
-                        echo "PPPPPPPPPP: Wrong(${#eth}) ethaddr="$eth"" > /dev/console
-                        exit
-                fi
+rm $TMPFILE
 
-                sed -i "s/f8:5e:3c:12:d7:84/"$eth"/g" /etc/config/network
-        elif [ "$f1" == wanaddr ]; then
-                if [ ${#eth} != 17 ]; then
-                        echo "PPPPPPPPPP: Wrong(${#eth}) wanaddr="$eth"" > /dev/console
-                        exit
-                fi
+#check first boot after factory
+if [[ ! -z "$(grep f8:5e:3c:12:d7:84 /etc/config/network)" ]]; then
+	if [ ${#lanmac} != 17 ]; then
+		echo "PPPPPPPPPP: Wrong(${#lanmac}) lanaddr="$lanmac"" > /dev/console
+		lanmac="00:1A:56:99:99:96"
+	fi
+	sed -i "s/f8:5e:3c:12:d7:84/"$lanmac"/g" /etc/config/network
 
-                sed -i "s/f8:5e:3c:12:d7:85/"$eth"/g" /etc/config/network
-        elif [ "$f1" == wifi0addr ]; then
-                if [[ -z "$(grep $eth /etc/config/wireless)" ]]; then
-                        sed -i "/default_radio0/a option macaddr '$eth'" /etc/config/wireless
-                fi
-        elif [ "$f1" == wifi1addr ]; then
-                if [[ -z "$(grep $eth /etc/config/wireless)" ]]; then
-                        sed -i "/default_radio1/a option macaddr '$eth'" /etc/config/wireless
-                fi
-        fi
-done
+	if [ ${#wanmac} != 17 ]; then
+		echo "PPPPPPPPPP: Wrong(${#wanmac}) wanaddr="$wanmac"" > /dev/console
+		wanmac="00:1A:56:99:99:97"
+	fi
+	sed -i "s/f8:5e:3c:12:d7:85/"$wanmac"/g" /etc/config/network
 
-#echo "PARKIS confirm ethaddr=${eth}" > /dev/console
+	cp /root/wireless.conf /etc/config/wireless
+	sed -i "s/00:1A:56:99:99:99/"$ra0mac"/g" /etc/config/wireless
+	sed -i "s/00:1A:56:99:99:98/"$ra1mac"/g" /etc/config/wireless
+fi
 
-#if [ $eth == *"0" ]; then
-#       exit
-#fi
