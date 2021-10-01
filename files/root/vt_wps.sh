696b719a58a527523be794854994e6eb18a533f5
Ubuntu 64-bit 20.04.2.0 <ispark65@naver.com>
2021-10-01 09:16:55 +0900

    [upgrade] using /root/config/wps-mode-always-on ...

--- files/root/vt_wps.sh
+++ files/root/vt_wps.sh
@@ -9,21 +9,21 @@ if [ -z "$1" ]; then
 fi
 
 if [[ -e /root/config/wps-timeout ]]; then
-	TMOUT=$(cat /root/config/wps-timeout)
+	wpstmout=$(cat /root/config/wps-timeout)
 else
-	TMOUT=120
+	wpstmout=120
 fi
-if [[ ! -z "${TMOUT//[0-9]/}" ]]; then
-	TMOUT=120
+if [[ ! -z "${wpstmout//[0-9]/}" ]]; then	# check number
+	wpstmout=120
 fi
-if [[ $TMOUT -lt 120 ]]; then
-	TMOUT=120
+if [[ $wpstmout -lt 120 ]]; then
+	wpstmout=120
 fi
 
-SLEEPTIME=2
-let TMOUTCNT=TMOUT/SLEEPTIME
-let TMOUTCNTMOD=TMOUT%SLEEPTIME
-[ $TMOUTCNTMOD -gt 0 ] && let TMOUTCNT=TMOUTCNT+1
+wpssleeptime=2
+let wpstmoutcnt=wpstmout/wpssleeptime
+let wpstmoutcntmod=wpstmout%wpssleeptime
+[ $wpstmoutcntmod -gt 0 ] && let wpstmoutcnt=wpstmoutcnt+1
 
 if [ "$1" == "start" ]; then
 	logger -t WPS wps start command received
@@ -37,15 +37,15 @@ if [ "$1" == "start" ]; then
 		logger -t WPS "$socket" actived
 	done
 
-	sleep $SLEEPTIME
+	sleep $wpssleeptime
 	cnt=1
 	for socket in *; do
 		[ -S "$socket" ] || continue
-		for i in $(seq 1 $TMOUTCNT); do
+		for i in $(seq 1 $wpstmoutcnt); do
 			if [[ ! -z "$(hostapd_cli -i "$socket" wps_get_status | grep 'PBC Status: Active')" ]]; then
-				sleep $SLEEPTIME
+				sleep $wpssleeptime
 				let cnt=cnt+1
-				if [ $cnt == $TMOUTCNT ]; then
+				if [ $cnt == $wpstmoutcnt ]; then
 					logger -t WPS deactived with timeout
 					echo default-on > /sys/devices/platform/leds/leds/$WPSLED/trigger
 					exit 0
@@ -70,21 +70,32 @@ elif [ "$1" == "stop" ]; then
 		logger -t WPS "$socket" canceled
 	done
 elif [ ${1} == "mode" ] && [ ${2} == "on" ]; then
-	echo 1 > /root/config/wps-mode-always-on
 	echo "WPS always-on mode command..." > /dev/console
-	if [[ ! -z $(ps | grep "vt_wps.sh mode on") ]]; then
+	if [[ -e /root/config/wps-mode-always-on ]]; then
 		echo "already 'vt_wps.sh mode on'..." > /dev/console
 		logger -t WPS wps always-on mode command received again in always-on
 		return 0
 	else
 		logger -t WPS wps always-on mode command received
+		echo 1 > /root/config/wps-mode-always-on
 	fi
 
 	cur_active=0
 	WPSLED=$(ls /sys/devices/platform/leds/leds/)
-	echo timer > /sys/devices/platform/leds/leds/$WPSLED/trigger
-	while true; do
-echo "always-on again ..." > /dev/console
+	while [[ -e /root/config/wps-mode-always-on ]]; do
+		echo timer > /sys/devices/platform/leds/leds/$WPSLED/trigger
+		while [[ -e /root/config/wifi-disable ]]; do
+			sleep $wpssleeptime
+			if [[ ! -e /root/config/wps-mode-always-on ]]; then
+				echo default-on > /sys/devices/platform/leds/leds/$WPSLED/trigger
+				logger -t WPS exit-wps-always-on mode
+
+				# terminated always-on mode
+				exit 0
+			fi
+		done
+
+#echo "always-on again ..." > /dev/console
 		cd /var/run/hostapd
 		for socket in *; do
 			[ -S "$socket" ] || continue
@@ -92,14 +103,14 @@ echo "always-on again ..." > /dev/console
 			logger -t WPS "$socket" actived
 		done
 
-		sleep $SLEEPTIME
+		sleep $wpssleeptime
 		while true; do
 			cd /var/run/hostapd
 			for socket in *; do
 				[ -S "$socket" ] || continue
 				if [[ ! -z "$(hostapd_cli -i "$socket" wps_get_status | grep 'PBC Status: Active')" ]]; then
 					let cur_active=1
-					sleep $SLEEPTIME
+					sleep $wpssleeptime
 				fi
 			done
 
@@ -111,6 +122,7 @@ echo "always-on again ..." > /dev/console
 				done
 
 				echo default-on > /sys/devices/platform/leds/leds/$WPSLED/trigger
+				logger -t WPS exit-wps-always-on mode
 
 				# terminated always-on mode
 				exit 0
@@ -124,6 +136,7 @@ echo "always-on again ..." > /dev/console
 	done
 
 	echo default-on > /sys/devices/platform/leds/leds/$WPSLED/trigger
+	logger -t WPS terminate-wps-always-on mode
 elif [ ${1} == "mode" ] && [ ${2} == "off" ]; then
 	rm -rf /root/config/wps-mode-always-on
 	logger -t WPS terminate-wps-always-on mode command received
