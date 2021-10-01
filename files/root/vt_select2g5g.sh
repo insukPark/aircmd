5c2a658aaaf6278427ee1a88065a8ce3d4b43338
Ubuntu 64-bit 20.04.2.0 <ispark65@naver.com>
2021-10-01 09:14:39 +0900

    [upgrade] modify like web command

--- files/root/vt_select2g5g.sh
+++ files/root/vt_select2g5g.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+#read GPIO 17
 if [ $1 == '2g' ]; then
+	echo "2.4G, control GPIO 5" > /dev/console
 	uci set wireless.default_radio1.disabled='1'
+	uci set wireless.radio1.disabled='1'
 	uci del wireless.default_radio0.disabled
+	uci del wireless.radio0.disabled
 	uci commit wireless
 	/sbin/wifi reload
 elif [ $1 == '5g' ]; then
+	echo "5G, control GPIO 5" > /dev/console
 	uci set wireless.default_radio0.disabled='1'
+	uci set wireless.radio0.disabled='1'
 	uci del wireless.default_radio1.disabled
+	uci del wireless.radio1.disabled
 	uci commit wireless
 	/sbin/wifi reload
 fi
