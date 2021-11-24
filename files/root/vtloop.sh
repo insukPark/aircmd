a2dc516442683612b6f8e03832186b249af5325a
Ubuntu 64 - 20.04.2 <ispark65@naver.com>
2021-11-24 14:31:37 +0900

    [upgrade] support wps command etc...

--- files/root/vtloop.sh
+++ files/root/vtloop.sh
@@ -19,6 +19,18 @@ if [[ -e /root/config/wps-mode-always-on ]]; then
 fi
 
 while true; do
+
+# /root/vt_wps.sh start || /root/vt_wps.sh mode on
+	if [[ -e /var/vt_wps_command ]]; then
+		wpscmd=$(cat /var/vt_wps_command)
+		rm -rf /var/vt_wps_command
+		if [[ $wpscmd == "start" ]]; then
+			/root/vt_wps.sh start &
+		elif [[ $wpscmd == "modeon" ]]; then
+			/root/vt_wps.sh mode on &
+		fi
+	fi
+
 # off 2.4G/5G LED for non-using wifi
 #	if [[ ! -z "$(uci show wireless | grep "wireless.default_radio0.disabled='1'")" ]]; then
 	if [[ ! -z "$(uci show wireless | grep "radio0.disabled='1'")" ]]; then
@@ -68,9 +80,19 @@ while true; do
 # check selection switch and apply 2.4G/5G only
 	if [ ! -e /root/config/wifi-disable ]; then
 		# read GPIO 17, maybe low means 2G && high means 5G
-		if [ -e /root/2g-switch ]; then
+		if [ ! -e /sys/class/gpio/gpio17 ]; then
+			echo 17 > /sys/class/gpio/export
+			echo in > /sys/class/gpio/gpio17/direction
+		fi
+		if [ ! -e /sys/class/gpio/gpio5 ]; then
+			echo 5 > /sys/class/gpio/export
+			echo out > /sys/class/gpio/gpio5/direction
+		fi
+
+		if [[ "$(cat /sys/class/gpio/gpio17/value)" -eq 1 ]]; then # 1 means 2.4G  #if [ -e /root/2g-switch ]; then
 			if [ $select2gswitchcnt -eq 1 ]; then
-echo "2.4G ! control GPIO 5 to low" > /dev/console
+echo "2.4G ! control GPIO 5 to high" > /dev/console
+				echo 1 > /sys/class/gpio/gpio5/value
 				uci set wireless.default_radio1.disabled='1'
 				uci set wireless.radio1.disabled='1'
 		 		uci del wireless.default_radio0.disabled
@@ -89,7 +111,8 @@ echo "2.4G ! control GPIO 5 to low" > /dev/console
 			fi
 		else
 			if [ $select5gswitchcnt -eq 1 ]; then
-echo "5G ! control GPIO 5 to high" > /dev/console
+echo "5G ! control GPIO 5 to low" > /dev/console
+				echo 0 > /sys/class/gpio/gpio5/value
 				uci set wireless.default_radio0.disabled='1'
 				uci set wireless.radio0.disabled='1'
 				uci del wireless.default_radio1.disabled
